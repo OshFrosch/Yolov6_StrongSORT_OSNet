539ad7a89ece636a9539b275a8ceb43a9c8078da
Joshua Kraft <nathan.joshua.kraft@gmx.de>
2023-01-06 15:56:20 +0100

    fixes numer of columns for export types

--- trackers/strong_sort/reid_multibackend.py
+++ trackers/strong_sort/reid_multibackend.py
@@ -167,19 +167,19 @@ class ReIDDetectMultiBackend(nn.Module):
         def export_formats():
             # YOLOv5 export formats
             x = [
-                ['PyTorch', '-', '.pt', True],
-                ['TorchScript', 'torchscript', '.torchscript', True],
-                ['ONNX', 'onnx', '.onnx', True],
-                ['OpenVINO', 'openvino', '_openvino_model', False],
-                ['TensorRT', 'engine', '.engine', True],
-                ['CoreML', 'coreml', '.mlmodel', False],
-                ['TensorFlow SavedModel', 'saved_model', '_saved_model', True],
-                ['TensorFlow GraphDef', 'pb', '.pb', True],
-                ['TensorFlow Lite', 'tflite', '.tflite', False],
-                ['TensorFlow Edge TPU', 'edgetpu', '_edgetpu.tflite', False],
-                ['TensorFlow.js', 'tfjs', '_web_model', False],
+                ['PyTorch', '-', '.pt', True, True],
+                ['TorchScript', 'torchscript', '.torchscript', True, True],
+                ['ONNX', 'onnx', '.onnx', True, True],
+                ['OpenVINO', 'openvino', '_openvino_model', True, False],
+                ['TensorRT', 'engine', '.engine', False, True],
+                ['CoreML', 'coreml', '.mlmodel', True, False],
+                ['TensorFlow SavedModel', 'saved_model', '_saved_model', True, True],
+                ['TensorFlow GraphDef', 'pb', '.pb', True, True],
+                ['TensorFlow Lite', 'tflite', '.tflite', True, False],
+                ['TensorFlow Edge TPU', 'edgetpu', '_edgetpu.tflite', False, False],
+                ['TensorFlow.js', 'tfjs', '_web_model', False, False],
                 ['PaddlePaddle', 'paddle', '_paddle_model', True, True],]
-            return pd.DataFrame(x, columns=['Format', 'Argument', 'Suffix', 'GPU'])
+            return pd.DataFrame(x, columns=['Format', 'Argument', 'Suffix', 'CPU', 'GPU'])
 
         sf = list(export_formats().Suffix)  # export suffixes
         check_suffix(p, sf)  # checks
